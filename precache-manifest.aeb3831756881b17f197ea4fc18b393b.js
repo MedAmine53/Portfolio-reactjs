9724c85fcab50e0ce6b6a9fb134469a7a828a6fd
TEMMAR Mohamed Amine <76709369+MedAmine53@users.noreply.github.com>
2023-02-18 21:54:26 +0100

    Add files via upload

---
+++ precache-manifest.aeb3831756881b17f197ea4fc18b393b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fb9dc5e769c5cff9779",
+    "url": "/static/css/main.7aad6427.chunk.css"
+  },
+  {
+    "revision": "0fb9dc5e769c5cff9779",
+    "url": "/static/js/main.0fb9dc5e.chunk.js"
+  },
+  {
+    "revision": "3a6221d47b2c54afecdd",
+    "url": "/static/js/1.3a6221d4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a545c40b2775647dad7919df1cd907f4",
+    "url": "/static/media/me.a545c40b.jpg"
+  },
+  {
+    "revision": "a54c6f23c6ad844463124041c023f799",
+    "url": "/static/media/CV.a54c6f23.pdf"
+  },
+  {
+    "revision": "446959922e0bf7ca4ab8e4c952621a6c",
+    "url": "/static/media/prj1.44695992.jpeg"
+  },
+  {
+    "revision": "bca0ba36502c0f956d7b0e83ce77a469",
+    "url": "/static/media/prj2.bca0ba36.webp"
+  },
+  {
+    "revision": "aa8eb579c89771f02de43f5ac52b9cff",
+    "url": "/static/media/prj3.aa8eb579.webp"
+  },
+  {
+    "revision": "90683044c8bf22cfe3087606a83413db",
+    "url": "/static/media/bg.90683044.jpg"
+  },
+  {
+    "revision": "2c6d135d9a2ab4245c63e64dc4ced9c2",
+    "url": "/index.html"
+  }
+];
